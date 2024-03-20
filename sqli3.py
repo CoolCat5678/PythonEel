a060b7b5b74e5123390739d9cf772db4f59e84a0
Leo <CoolCat5678@users.noreply.github.com>
2024-03-20 16:50:02 +0800

    add select script

--- sqli3.py
+++ sqli3.py
@@ -4,11 +4,33 @@ import sqlite3
 con = sqlite3.connect("toten.db")
 cur = con.cursor()
 
+# 控制是否需要commit rollback
+con.isolation_level = None
+
+@eel.expose
+def load():
+    ID = cur.execute('SELECT ID FROM Customer')
+    return ID.fetchall()
+# PRAGMA table_info(table_name);
+
 @eel.expose
 def SqlExec(word):
+    r = ''
     res = cur.execute(word)
-    return f'{res.fetchone()}'
+    for i in res.fetchall():
+        r += str(i)
+        
+    return f'{r}'
 
+@eel.expose
+def SelectID(ID):
+    r = ''
+    strSQL = f"SELECT C.ID, T.TradeDate, T.Amount FROM Customer C LEFT JOIN TradeHis T ON C.ID=T.ID WHERE C.ID='{ID}'"
+    res = cur.execute(strSQL)
+    for i in res.fetchall():
+        r += str(i) + '\r' 
+        
+    return f'{r}'
 
 eel.init('web')
-eel.start('main.html',size = (400,600))
+eel.start('main.html',size = (1000,600))
--- web/main.js
+++ web/main.js
@@ -1,5 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    result = eel.load()()
+    result.then(
+        function(val)
+        {
+            val.forEach(element => {
+                document.querySelector('#IDselect').innerHTML += `<option value="${element[0]}">${element[0]}</option>`
+            });
+        }
+    );
+  });
+
 async function commit(){ 
     var commandText = document.querySelector('#SQLcommand').value
     result = await eel.SqlExec(commandText)()
+    document.querySelector('#output').textContent = result
+}
+
+async function SelectID(SelectID){
+    result = await eel.SelectID(SelectID)()
     document.querySelector('#output').textContent = result
 }
